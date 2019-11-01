8d1ec4666f260cf1e12e7b5ef53d1a8357fb3c8d
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2019-11-01 14:10:23 +0100

    Updated docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,7 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
     const options = new DocumentBuilder()
-        .setTitle('Nest.js example API')
+        .setTitle('My Doctor API')
+        .setLicense('MIT License','ttps://opensource.org/licenses/MIT')
+        .setContactEmail('maciej.krzywdaa@gmail.com')
         .setDescription('API Documentation')
         .setVersion('1.0')
         .addBearerAuth()
